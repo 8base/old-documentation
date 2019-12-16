45896de6f37c25ce677039d2e09f9644b8582c2c
Sebastian Scholl <sebscholl@gmail.com>
2019-12-15 19:26:12 -0600

    Fixed favicon and disallowed sephia theme

--- src/layouts/partials/RightSidebar.vue
+++ src/layouts/partials/RightSidebar.vue
@@ -1,11 +1,20 @@
 <template>
-    <div v-if="subtitles.length > 0 && subtitles[0].depth !== 3" class="sidebar sidebar--right hide-for-small">
+    <div class="sidebar sidebar--right hide-for-small">
         <h3>On this page</h3>
+
+        <!-- Render age sections -->
         <ul v-if="subtitles.length" class="menu-item submenu">
             <li class="submenu__item" :class="'submenu__item-depth-' + subtitle.depth" v-for="(subtitle, index) in subtitles" :key="subtitle.value + index">
             <a class="submenu__link" :href="subtitle.anchor">
                 {{ subtitle.value }}
             </a>
+            </li>
+        </ul>
+
+        <!-- When sections are empty -->
+        <ul v-else class="menu-item submenu">
+            <li class="submenu__item">
+              No sections...
             </li>
         </ul>
     </div>
@@ -15,6 +24,11 @@
 export default {
   props: {
     subtitles: { type: Array, default: () => [] },
+  },
+  computed: {
+    hasSections() {
+      return this.subtitles.length > 0 && this.subtitles[0].depth !== 3;
+    }
   }
 }
 </script>
